58d8a955e76089790de3a5cb26176a4313e1eaca
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2017-01-25 11:45:25 -0300

    .

--- resources/views/backend/partials/loyaltycard.blade.php
+++ resources/views/backend/partials/loyaltycard.blade.php
@@ -1,1 +1,63 @@
-<p>oi</p>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Farmeria</title>
+  <meta charset="UTF-8">
+
+  <link href="https://fonts.googleapis.com/css?family=Yantramanav:100,300,400,500,700,900" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div id="gameboard">
+    <h2>*Consulte o regulamento em nossa loja</h2>
+    <div class="card image-cropper">
+      <img id="imagem" src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+    <div class="card image-cropper">
+      <img src="/assets/images/logoFarmeria.jpg">
+    </div>
+
+  </div>
+  <div class="icone pessoa">
+    <img src="/assets/images/IconePessoaFarmeria.jpg">
+  </div>
+
+  <div class="icone telefone">
+    <img src="/assets/images/IconeTelefoneFarmeria.jpg">
+  </div>
+
+</body>
+</html>
