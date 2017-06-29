088f80fd19804282fbb72b57b3d3ef71499ad44e
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2017-06-29 15:54:34 -0300

    adição do table responsive

--- resources/views/usuarios/show_guest.blade.php
+++ resources/views/usuarios/show_guest.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-body">
+                    <div class="panel-body table-responsive">
                         <table class="table table-bordered table-striped">
                             <tr>
                                 <th>Nome</th>
