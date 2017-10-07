b607b8bfa86667dfa7399c88b7957ea8edc611c7
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2017-10-07 12:14:14 -0300

    correcao imagens

--- resources/views/backend/partials/loyaltycard.blade.php
+++ resources/views/backend/partials/loyaltycard.blade.php
@@ -2,36 +2,36 @@
 <div id="gameboard">
   <div class="col-md-12">
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
   </div>
   <div class="col-md-12">
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
     <div class="col-md-2 ponto">
-      <img class="img-responsive img-circle center-block" src="/assets/images/logoFarmeria.jpg">
+      <img class="img-responsive img-circle center-block" src="{{asset('assets/images/logoFarmeria.jpg')}}">
     </div>
   </div>
 </div>
@@ -39,21 +39,18 @@
 
 <script>
   var indexes = {{ $item->card()->first()->pontos->count() }};
-  console.log(indexes);
   var pontos = {!! $item->pontosJson() !!};
-  console.log(pontos);
   var operadores = {};
   function parse2dict(item, index)
   {
     operadores[item.id] = item.name;
   }
   {!! $operadores !!}.forEach(parse2dict);
-  console.log(operadores);
   var cols = document.getElementsByClassName('ponto');
   for (var i = 0; i < indexes ; i++)
   {
     cols[i].style.fontSize = "x-small";
-    cols[i].innerHTML = '<img class="img-responsive img-circle center-block" src="/assets/images/check-tick-icon-14150.png">';
+    cols[i].innerHTML = '<img class="img-responsive img-circle center-block" src="{{asset('/assets/images/check-tick-icon-14150.png')}}">';
     cols[i].innerHTML += "<p>Cupom fiscal:" + pontos[i].cupomfiscal + "</p><p>Data da compra: " + pontos[i].data_compra + "</p><p>Operador:" + operadores[pontos[i].operador_id] + "</p>";
   }
 
